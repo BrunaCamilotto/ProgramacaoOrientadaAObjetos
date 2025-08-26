5df6b14305a8310d27403193456708348dd78cec
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2025-08-26 18:17:49 -0300

    Implementa
    Algoritmo de soma simples.

---
+++ Exercicios Basicos/Soma/src/com/ifsc/tds/Main.java
@@ -0,0 +1,15 @@
+package com.ifsc.tds;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int primeiroNumero = 3, segundoNumero = 5;
+		float soma = 0f;
+		
+		soma = primeiroNumero + segundoNumero;
+		
+		System.out.println("Soma é: " + soma);
+		
+	}
+
+}
