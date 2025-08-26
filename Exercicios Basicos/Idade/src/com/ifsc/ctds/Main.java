0192fe5ec638c831ecf3c368e4bfb32d11ea32f3
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2025-08-26 18:16:23 -0300

    Implementa
    Algoritmo básico de variável.

---
+++ Exercicios Basicos/Idade/src/com/ifsc/ctds/Main.java
@@ -0,0 +1,17 @@
+package com.ifsc.ctds;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int idade = 24;
+		float salario = 125.50f;
+		char letra = 'A';
+
+		System.out.println("Teste!");
+		System.out.println("Sua idade é: " + idade);
+		System.out.println("Seu salario é: " + salario);
+		System.out.println("Sua letra é: " + letra);
+	}
+
+}
